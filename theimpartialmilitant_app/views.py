373d04e62e12757359cb6977eac65faec29c1d3c
Ana Paula Carvalho <ana.carvalho@moneyfarm.com>
2017-01-27 00:04:54 +0000

    sub app with scripts added

--- theimpartialmilitant_app/views.py
+++ theimpartialmilitant_app/views.py
@@ -1,5 +1,9 @@
 from django.shortcuts import render
+from scripts import parliament_data as p
 
 # Create your views here.
 def index(request):
-    return render(request, 'project_name_app/index.html', {})
+    context = {
+    "issues":p.get_data()
+    }
+    return render(request, 'theimpartialmilitant_app/index.html', context)
